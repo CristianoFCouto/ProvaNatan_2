13fedec4544ee0c5658857be84cb33c906ae9781
CristianoFCouto <cristianodocouto@hotmail.com>
2024-12-12 18:29:14 -0300

    Create ContaAdicional.java

---
+++ sistemaBancario/src/contas/ContaAdicional.java
@@ -0,0 +1,7 @@
+package contas;
+
+public class ContaAdicional extends Conta {
+    public ContaAdicional(String usuario, double saldo, double limite) {
+        super(usuario,saldo,limite);
+    }
+}
