0c8d4462401cbf631711d280c13066c33a9adf38
CristianoFCouto <cristianodocouto@hotmail.com>
2024-12-12 18:29:17 -0300

    Create ContaCorrentePrincipal.java

---
+++ sistemaBancario/src/contas/ContaCorrentePrincipal.java
@@ -0,0 +1,7 @@
+package contas;
+
+public class ContaCorrentePrincipal extends Conta {
+	public ContaCorrentePrincipal(String titular, double saldo, double limite, double especial) {
+		super(titular, saldo, limite,especial);
+	}
+}
